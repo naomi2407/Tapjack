c9f55a7051322400c07de96628fccab9b0057178
naomi2407 <43143897+naomi2407@users.noreply.github.com>
2018-10-08 10:06:02 -0400

    Custom Arrays for 10 different webpages
    Creating 10 different HTML and JS files for 10 different links, and preloading only those images determined for every individual link per JS file.

---
+++ CustomArray 10 pages/puzzle3.js
@@ -0,0 +1,91 @@
+var numimages = 8; //The number of images
+var images = []; // Made array
+var currentframe = 0;
+var state = 0;
+var affirm;
+var song;
+var yesh;
+
+
+var des_img = 0;
+
+function preload() {
+for (var i = 0; i< numimages; i++) {
+img0 =loadImage('images/support1.jpg');
+img2 =loadImage('images/support3.jpg');
+img3 =loadImage('images/support4.jpg');
+img5 =loadImage('images/support6.jpg');
+img8 =loadImage('images/support9.jpg');
+img9 =loadImage('images/support10.jpg');
+img13 =loadImage('images/support14.jpg');
+master2 =loadImage('masterpuzzle/3.jpg');
+
+
+images = [master2, img5, img8, img3, img13, img0, img2, img9];	
+
+
+affirm = loadImage("Commandnegative.png");
+//soundFormats('mp3');
+song = loadSound('NO.mp3');
+yesh = loadSound('Yay.mp3');
+	
+}    
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(3);
+
+	
+		
+}
+function draw (){
+    image(images[currentframe], 0,0, width, height);
+    currentframe++ //next frame
+    if (currentframe == images.length) {
+        currentframe=0; //Return to first frame
+    }
+}
+
+
+function resizeWindow(){
+    // change canvas size
+    createCanvas(windowWidth, windowHeight);
+    
+}
+
+function touchStarted(){ //on click
+    if(currentframe== des_img)
+    {    //if clicked when desired image is displayed
+    noLoop();
+	state=1;
+	yesh.play();
+
+
+
+    }//stop the loop
+//stop();
+    else if((currentframe!= des_img)&&(state==0))
+        {
+			song.play();
+			image(affirm, width/2 - 250, height/2 - 250, 500, 500);
+			
+        }
+}
+
+//function mouseReleased(){
+//    loop();
+//}
+
+//function stop() {
+  
+    //currentframe =! 0;
+    //currentframe =! 1;
+    //currentframe = 2;
+    //currentframe =! 3;
+    //currentframe =! 4;
+    //}
+
+
+
+//
+
