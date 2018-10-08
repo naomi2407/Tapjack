ef61f918650d711e6ea6620297985535826d8c4b
naomi2407 <43143897+naomi2407@users.noreply.github.com>
2018-10-08 10:11:13 -0400

    Styling of webpage 1
    A document without the overall aesthetic of the webpage, but without functioning buttons that lead to the individual web pages for every loop.

---
+++ Tapjackstyled1_without working links/puzzle1.js
@@ -0,0 +1,93 @@
+var numimages = 8; //The number of images
+var images = []; // Made array
+var currentframe = 0;
+var state = 0;
+var affirm;
+var song;
+var yesh;
+
+
+var des_img = 0;
+
+function preload() {
+for (var i = 0; i< numimages; i++) {
+img0 =loadImage('images/support1.jpg');
+img1 =loadImage('images/support2.jpg');
+img2 =loadImage('images/support3.jpg');
+img3 =loadImage('images/support4.jpg');
+img4 =loadImage('images/support5.jpg');
+img5 =loadImage('images/support6.jpg');
+img6 =loadImage('images/support7.jpg');
+img7 =loadImage('images/support8.jpg');
+master0 =loadImage('masterpuzzle/1.jpg');
+
+
+images = [master0, img1, img2, img3, img4, img5, img6, img7];	
+
+
+affirm = loadImage("Commandnegative.png");
+//soundFormats('mp3');
+song = loadSound('NO.mp3');
+yesh = loadSound('Yay.mp3');
+	
+}    
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(3);
+
+
+	
+		
+}
+function draw (){
+    image(images[currentframe], 0,0, width, height);
+    currentframe++ //next frame
+    if (currentframe == images.length) {
+        currentframe=0; //Return to first frame
+    }
+}
+
+
+function resizeWindow(){
+    // change canvas size
+    createCanvas(windowWidth, windowHeight);
+    
+}
+
+function touchStarted(){ //on click
+    if(currentframe== des_img)
+    {    //if clicked when desired image is displayed
+    noLoop();
+	state=1;
+	yesh.play();
+
+
+
+    }//stop the loop
+//stop();
+    else if((currentframe!= des_img)&&(state==0))
+        {
+			song.play();
+			image(affirm, width/2 - 250, height/2 - 250, 500, 500);
+			
+        }
+}
+
+//function mouseReleased(){
+//    loop();
+//}
+
+//function stop() {
+  
+    //currentframe =! 0;
+    //currentframe =! 1;
+    //currentframe = 2;
+    //currentframe =! 3;
+    //currentframe =! 4;
+    //}
+
+
+
+//
+
---
+++ Tapjackstyled1_without working links/puzzle4.js
@@ -0,0 +1,91 @@
+var numimages = 8; //The number of images
+var images = []; // Made array
+var currentframe = 0;
+var state = 0;
+var affirm;
+var song;
+var yesh;
+
+
+var des_img = 0;
+
+function preload() {
+for (var i = 0; i< numimages; i++) {
+img2 =loadImage('images/support3.jpg');
+img6 =loadImage('images/support7.jpg');
+img8 =loadImage('images/support9.jpg');
+img10 =loadImage('images/support11.jpg');
+img12 =loadImage('images/support13.jpg');
+img14 =loadImage('images/support15.jpg');
+img15 =loadImage('images/support16.jpg');
+master3 =loadImage('masterpuzzle/4.jpg');
+
+
+images = [master3, img2, img6, img8, img10, img12, img14, img15];	
+
+
+affirm = loadImage("Commandnegative.png");
+//soundFormats('mp3');
+song = loadSound('NO.mp3');
+yesh = loadSound('Yay.mp3');
+	
+}    
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(3);
+
+	
+		
+}
+function draw (){
+    image(images[currentframe], 0,0, width, height);
+    currentframe++ //next frame
+    if (currentframe == images.length) {
+        currentframe=0; //Return to first frame
+    }
+}
+
+
+function resizeWindow(){
+    // change canvas size
+    createCanvas(windowWidth, windowHeight);
+    
+}
+
+function touchStarted(){ //on click
+    if(currentframe== des_img)
+    {    //if clicked when desired image is displayed
+    noLoop();
+	state=1;
+	yesh.play();
+
+
+
+    }//stop the loop
+//stop();
+    else if((currentframe!= des_img)&&(state==0))
+        {
+			song.play();
+			image(affirm, width/2 - 250, height/2 - 250, 500, 500);
+			
+        }
+}
+
+//function mouseReleased(){
+//    loop();
+//}
+
+//function stop() {
+  
+    //currentframe =! 0;
+    //currentframe =! 1;
+    //currentframe = 2;
+    //currentframe =! 3;
+    //currentframe =! 4;
+    //}
+
+
+
+//
+
